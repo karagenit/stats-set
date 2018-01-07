7bae1eff29528334f8f6dcc9d88bce333ff216b2
Caleb Smith <karagenit@outlook.com>
2018-01-07 13:00:41 -0500

    Add Function Skeletons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+function highlightCard(card) {
+    $(card).removeClass("btn-secondary").addClass("btn-primary");
+}
+
+function clearCard(card) {
+    $(card).removeClass("btn-primary").addClass("btn-primary");
+}
+
+function clearAllCards() {
+
+}
+
+function randomizeCard(card) {
+
+}
+
+function randomizeAllCards() {
+
+}
+
+function checkSet() {
+
+}
+
+// Sets up the board, generates each card's state
 $(document).ready(function() {
     //init cards
 });
 
+// Handles clicks on buttons
 $("#btn1").click(function() {
-    $(this).removeClass("btn-secondary").addClass("btn-primary");
+    highlightCard(this);
+    checkSet();
 });
