74e35142ea0c1ef5a96e1726e50b74f43cd71e89
Caleb Smith <karagenit@outlook.com>
2018-01-08 01:16:34 +0000

    Add Styling to Score Page
    Closes #2

--- src/score.php
+++ src/score.php
@@ -5,9 +5,28 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
+
+    <title>Stats Set</title>
+</head>
 <body>
-    <p>Your score was: <?php echo $_GET['score'] ?></p>
-    <p><a href="index.html">Click to play again</a></p>
+    <div class="center">
+        <h1>Game Over!</h1>
+        <p>
+            Your score was: <b><?php echo $_GET['score'] ?></b>
+            <br><br>
+            <a href="index.html" class="btn btn-outline-success">Click to play again</a>
+        </p>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
 </body>
 </html>
